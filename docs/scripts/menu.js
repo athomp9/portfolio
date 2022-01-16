36b42eb07b385c1d1a78ec2d64078ff0e94bf794
athomp9 <athomp9@artic.edu>
2022-01-16 12:44:03 -0600

    add menu changes

--- docs/scripts/menu.js
+++ docs/scripts/menu.js
@@ -23,14 +23,14 @@ let mainMenu = `
 
 <nav id="slide-menu"> 
 <ul>
-  <li><a href="${prefix}/">ArthurArthur</a></li>
-  <li><a href="${prefix}/print/">Print & Design</a></li>
-  <li><a href="${prefix}/performance/">Performance</a></li>
-  <li><a href="${prefix}/fibers/">Fibers</a></li>
-  <li><a href="${prefix}/audio-works/">Sound</a></li>
-  <li><a href="${prefix}/fibers/plastic_gallery">Plastic</a></li>
-  <li><a href="${prefix}/process/">Process</a></li>
-  <li><a href="${prefix}/about-me/">About Me</a></li>
+  <li class="home"><a href="${prefix}/">HOME</a></li>
+  <li class="print"><a href="${prefix}/print/">PRINT & DESIGN</a></li>
+  <li class="perf"><a href="${prefix}/performance/">PERFORMANCE</a></li>
+  <li class="fibers"><a href="${prefix}/fibers/">FIBERS</a></li>
+  <li class="sound"><a href="${prefix}/audio-works/">SOUND</a></li>
+  <li class="plastic"><a href="${prefix}/fibers/plastic_gallery">PLASTIC</a></li>
+  <li class="process"><a href="${prefix}/process/">PROCESS</a></li>
+  <li class="about-me"><a href="${prefix}/about-me/">ABOUT ME</a></li>
 </ul>
 </nav>
 `
