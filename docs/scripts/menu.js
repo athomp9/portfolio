bfc45e39b0d0438d304f465b212b6ecce0474c59
athomp9 <athomp9@artic.edu>
2022-03-24 19:35:06 -0500

    add ceramics tab, hells, and purse

--- docs/scripts/menu.js
+++ docs/scripts/menu.js
@@ -10,10 +10,11 @@ let prefix = setPrefix(REPO_NAME, ROOT_IS_DOCS);
 let mainMenu = `
 <ul id="work-menu">
 <li><a href="${prefix}/">ArthurArthur</a></li>
-<li><a href="${prefix}/print/">Print & Design</a></li>
+<li><a href="${prefix}/print/">Print & Painting</a></li>
 <li><a href="${prefix}/performance/">Performance</a></li>
 <li><a href="${prefix}/fibers/">Fibers</a></li>
 <li><a href="${prefix}/audio-works/">Sound</a></li>
+<li><a href="${prefix}/ceramics/">Ceramics</a></li>
 <li><a href="${prefix}/fibers/plastic_gallery">Plastic</a></li>
 <li><a href="${prefix}/process/">Process</a></li>
 <li><a href="${prefix}/about-me/">About Me</a></li>
@@ -24,10 +25,11 @@ let mainMenu = `
 <nav id="slide-menu"> 
 <ul>
   <li class="home"><a href="${prefix}/">HOME</a></li>
-  <li class="print"><a href="${prefix}/print/">PRINT & DESIGN</a></li>
+  <li class="print"><a href="${prefix}/print/">PRINT & PAINTING</a></li>
   <li class="perf"><a href="${prefix}/performance/">PERFORMANCE</a></li>
   <li class="fibers"><a href="${prefix}/fibers/">FIBERS</a></li>
   <li class="sound"><a href="${prefix}/audio-works/">SOUND</a></li>
+  <li><a href="${prefix}/ceramics/">CERAMICS</a></li>
   <li class="plastic"><a href="${prefix}/fibers/plastic_gallery">PLASTIC</a></li>
   <li class="process"><a href="${prefix}/process/">PROCESS</a></li>
   <li class="about-me"><a href="${prefix}/about-me/">ABOUT ME</a></li>
